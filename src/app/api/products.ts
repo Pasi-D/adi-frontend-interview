f400f3c4c108d101987859310a5a5bdb6e1b9a55
Pasindu Dilshan <pasindu_dilshan@outlook.com>
2024-09-12 19:09:51 +0530

    Chore: Configure products endpoint via .env

--- src/app/api/products.ts
+++ src/app/api/products.ts
@@ -2,7 +2,7 @@ import { IProduct } from "../types";
 
 export const getProduct = async (id: string): Promise<IProduct | null> => {
   try {
-    const res = await fetch(`https://dummyapi.online/api/products/${id}`);
+    const res = await fetch(`${process.env.NEXT_PUBLIC_PRODUCTS_API}/products/${id}`);
     if (!res.ok) throw new Error("Failed to fetch product");
     return res.json();
   } catch (error) {
@@ -13,7 +13,7 @@ export const getProduct = async (id: string): Promise<IProduct | null> => {
 
 export const getProducts = async (): Promise<Array<IProduct> | null> => {
   try {
-    const res = await fetch(`https://dummyapi.online/api/products`);
+    const res = await fetch(`${process.env.NEXT_PUBLIC_PRODUCTS_API}/products`);
     if (!res.ok) throw new Error("Failed to fetch product");
     return res.json();
   } catch (error) {
