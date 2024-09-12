ba097d464069a88511df91ca1ada26117bd79379
Pasindu Dilshan <pasindu_dilshan@outlook.com>
2024-09-12 12:57:57 +0530

    Chore: Clean unused image

--- next.config.mjs
+++ next.config.mjs
@@ -5,10 +5,6 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "nextjs.org",
-      },
-      {
-        protocol: "https",
         hostname: "tailwindui.com",
       },
       {
